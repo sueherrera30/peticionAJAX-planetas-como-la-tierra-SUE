7d324d966848990a2ec1ea5fff69b7c1f444ce0b
sue martinez <sueherrera30@gmail.com>
2017-07-13 09:28:55 +0000

    correción de sintaxis

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,25 @@
-function getJSON(url) {
-	return new Promise(function (resolve, reject) {
-
+/**/
+function getJSON(url){
+	return new Promise(function(resolve, reject){
 		/*pedimos data al server*/
 		var ajax = new XMLHttpRequest();
-		/*almacenamos en variable nuestra ruta */
-		var url = url;
 		/*hacemos peticion de url através de verbo get???*/
-		ajax.open("GET", url);
+		ajax.open("GET",url);
 		/*hacemos la petición al servidor*/
 		ajax.send();
-		ajax.onreadystatechange = function () {
+		ajax.onreadystatechange = function (){
 			/*inicia condicional para validar promesa*/
-			if (ajax.readyState == 4) {
+			if(ajax.readyState == 4){
 				resolve(JSON.parse(ajax.responseText));
 			}
 		}
 	})
-};
+}
 
 /*llamamos a función que contiene promesa pasando como parametro la url de la funcion anterior */
+/*acciones basadas en otras acciones ,obteneos info global (1ra linea), despues info especifica, obtenemos json de un planeta en particular y despues se loguea.*/
 getJSON("data/earth-like-results.json")
+/*se define una función que se va a ejecutr hasta que se compruebe , y respete la asincronia */
+/*cuando se pasa como parametro mensaje, el resultado de la promesa exitosa se va inicializar y ejecutar gracias a esta función*/
 .then(function(mensaje){return getJSON(mensaje.results[0])})
-	.then(function(resultado){comsole.log(resultado)});
+.then(function(resultado){console.log(resultado)});
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 /*traemos express y path */
+/*express es un paquete, un servidor de js*/
 var express = require("express");
 var path = require("path");
-/*iniciamos a express*/
+/*ejecutamod a express y la guardamos en una variabl*/
 var app = express();
-
-/*endpoint*/
-app.get("/",(req,res)=>{
-	res.sendFile(__dirname + "/index.html");
-});
 /*haceos rutas estaticas*/
+/*app.use , nos dice que en un lugar hay una carpeta que quiero que se haga publica; 
+express.static: hará una ruta estatica de ciertos archivos que nosotros les demos,
+al usar path, ayuda a enviar files, archivos,enrealidad static no exixste es mas bien una ruta que express usara para poder acceder a esos archivos*/
 app.use("/data",express.static(__dirname + "/data"));
 app.use("/static",express.static(path.join(__dirname + "/assets")));
 app.use("/static",express.static(path.join(__dirname + "/node_modules")));
+/*endpoint*/
+/*metodos verbos http get, hace peticion , e este caso es a una accción hecha por una función ,cuando entremos a esta petición nos dara este archivo*/
+app.get("/",(req,res)=>{
+	res.sendFile(__dirname + "/index.html");
+});
+
 /*hacemos que nos escuche el puerto*/
 app.listen(8000);
