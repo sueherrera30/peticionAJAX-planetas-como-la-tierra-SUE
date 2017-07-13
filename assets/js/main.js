d6ef7c02ec2dd88decd5c7dbca05a72cc203d8ff
sue martinez <sueherrera30@gmail.com>
2017-07-13 13:07:10 +0000

    itera planetas e imprime en consola

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,41 @@
-/**/
-function getJSON(url){
-	return new Promise(function(resolve, reject){
-		/*pedimos data al server*/
+function getJSON(url) {
+	return new Promise(function (resolve, reject) {
+		/*pedimos data al server atraveés de un nuevo objeto  que se esta guardando en ajax*/
 		var ajax = new XMLHttpRequest();
-		/*hacemos peticion de url através de verbo get???*/
-		ajax.open("GET",url);
+		/*nos indita el tipo de petición que le haremos a la url que le daremos,la va preparando, aun no la manda.*/
+		ajax.open("GET", url);
 		/*hacemos la petición al servidor*/
 		ajax.send();
-		ajax.onreadystatechange = function (){
+		/*pregunta cambio de estado del ajax*/
+		ajax.onreadystatechange = function () {
 			/*inicia condicional para validar promesa*/
-			if(ajax.readyState == 4){
+			if (ajax.readyState == 4) {
 				resolve(JSON.parse(ajax.responseText));
 			}
 		}
 	})
 }
 
-/*llamamos a función que contiene promesa pasando como parametro la url de la funcion anterior */
+/* getJson("data....)---->llamamos a función que contiene promesa pasando como parametro la url de la funcion anterior */
 /*acciones basadas en otras acciones ,obteneos info global (1ra linea), despues info especifica, obtenemos json de un planeta en particular y despues se loguea.*/
-getJSON("data/earth-like-results.json")
-/*se define una función que se va a ejecutr hasta que se compruebe , y respete la asincronia */
+
+/*se define una función que se va a ejecutar hasta que se compruebe , y respete la asincronia */
 /*cuando se pasa como parametro mensaje, el resultado de la promesa exitosa se va inicializar y ejecutar gracias a esta función*/
-.then(function(mensaje){return getJSON(mensaje.results[0])})
-.then(function(resultado){console.log(resultado)});
+
+getJSON("data/earth-like-results.json")
+	.then(function (mensaje) {
+		    var listaPlanetas = mensaje.results;
+			return getJSON(listaPlanetas.forEach(function(planeta){
+				getJSON(planeta)
+				.then(function(resultadoPlaneta){
+					console.log(resultadoPlaneta);	
+				})
+			}))
+			
+		
+	})
+
+
+	.then(function (resultado) {
+		console.log(resultados)
+	});
